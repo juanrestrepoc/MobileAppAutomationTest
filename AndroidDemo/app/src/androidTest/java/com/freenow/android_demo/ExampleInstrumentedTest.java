32c3c5ebe65dbab54e5490683abaf1af9409430d
Juan Restrepo <juan.restrepo@MB-20000178.office.innogames.net>
2019-08-06 12:52:15 +0200

    Initial CircleCI configuration and example test

--- AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/ExampleInstrumentedTest.java
+++ AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/ExampleInstrumentedTest.java
@@ -1,9 +1,25 @@
 package com.freenow.android_demo;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.clearText;
+
+
+import com.freenow.android_demo.activities.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -14,13 +30,29 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
+@LargeTest
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.freenow.android_demo", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void failedLoginWithWrongCredentialsShowsMessageInSnackbar() throws InterruptedException {
+        onView(withId(R.id.edt_username))
+                .perform(click(),
+                        clearText(),
+                        typeTextIntoFocusedView("Hello"),
+                        closeSoftKeyboard());
+
+        onView(withId(R.id.edt_password))
+                .perform(click(),
+                        clearText(),
+                        typeTextIntoFocusedView("Hello"),
+                        closeSoftKeyboard());
+
+        onView(withId(R.id.btn_login)).perform(click());
+
+        onView(withText(R.string.message_login_fail)).check(matches(isDisplayed()));
     }
 }
