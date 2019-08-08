a8dc1cc4ca0a94e9ddf660cdbcd22595b04a52d4
Juan Restrepo <juan.restrepo@MB-20000178.office.innogames.net>
2019-08-08 18:00:57 +0200

    Added tests

--- AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/ExampleInstrumentedTest.java
+++ AndroidDemo/app/src/androidTest/java/com/freenow/android_demo/ExampleInstrumentedTest.java
@@ -2,26 +2,34 @@ package com.freenow.android_demo;
 
 import android.content.Context;
 
-import androidx.test.filters.LargeTest;
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.clearText;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.contrib.DrawerActions.close;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.Matchers.allOf;
+import static android.support.test.espresso.contrib.DrawerActions.open;
 
 import com.freenow.android_demo.activities.MainActivity;
 
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
@@ -30,29 +38,62 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4ClassRunner.class)
-@LargeTest
+@RunWith(AndroidJUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ExampleInstrumentedTest {
 
     @Rule
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
 
+    private String username;
+    private String password;
+    private String optionName;
+    private String optionPhoneNumber;
+
+    @Before
+    public void setTestData() {
+        username = "crazydog335";
+        password = "venture";
+        optionName = "Sarah Scott";
+    }
+
     @Test
-    public void failedLoginWithWrongCredentialsShowsMessageInSnackbar() throws InterruptedException {
+    public void loginUserLooksForSecondDriverOnListAndCallsDriver() throws Exception {
+
         onView(withId(R.id.edt_username))
                 .perform(click(),
                         clearText(),
-                        typeTextIntoFocusedView("Hello"),
+                        typeTextIntoFocusedView(username),
                         closeSoftKeyboard());
 
         onView(withId(R.id.edt_password))
                 .perform(click(),
                         clearText(),
-                        typeTextIntoFocusedView("Hello"),
+                        typeTextIntoFocusedView(password),
                         closeSoftKeyboard());
 
         onView(withId(R.id.btn_login)).perform(click());
 
-        onView(withText(R.string.message_login_fail)).check(matches(isDisplayed()));
+        Thread.sleep(5000);
+
+        onView(withId(R.id.drawer_layout)).perform(open());
+
+        onView(withId(R.id.nav_username))
+                .check(matches(withText(username)));
+        onView(withId(R.id.drawer_layout)).perform(close());
+
+        Thread.sleep(5000);
+
+        onView(withId(R.id.textSearch))
+                .perform(click(),
+                        clearText(),
+                        typeTextIntoFocusedView("sa"),
+                        closeSoftKeyboard());
+        onView(withText(optionName))
+                .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
+                .check(matches(isDisplayed()));
+        onView(withText(optionName))
+                .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
+                .perform(click());
     }
 }
